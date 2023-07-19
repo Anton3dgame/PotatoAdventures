c085c5074472087768aeef7454b96cb747d8a662
Anton_P <anton.paschke@st.ovgu.de>
2023-07-19 11:30:28 +0200

    hebel fix

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -7,18 +7,55 @@ public class ButtonBehaviour : MonoBehaviour
 
     [SerializeField] public GameObject door;
 
+    private bool isButtonActivated = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (!isButtonActivated)
+        {
+            isButtonActivated = true;
+            Vector3 localscale = transform.localScale;
+            localscale.x *= -1f;
+            transform.localScale = localscale;
+            if (door.activeSelf)
+            {
+                door.SetActive(false);
+            }
+            else
+            {
+                door.SetActive(true);
+            }
+        }
+    } 
+
+    /* if (collision.gameObject.CompareTag("Lever1"))
+        {
+
+        }
+        else if (collision.gameObject.CompareTag("Lever2"))
+        {
+
+        }
+        else if (collision.gameObject.CompareTag("Lever3"))
+        {
+
+        }
+        else if (collision.gameObject.CompareTag("Lever4"))
+        {
+
+        }
+
+
         Vector3 localscale = transform.localScale;
-        localscale.x *= -1f;
+    localscale.x *= -1f;
         transform.localScale = localscale;
         if (door.activeSelf)
         {
             door.SetActive(false);
         }
         else
-        {
-            door.SetActive(true);
-        }
-    }
+{
+    door.SetActive(true);
+}*/
 }
