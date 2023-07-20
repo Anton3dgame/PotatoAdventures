1646495d58172503ae5c2fb163a85359d639ac15
Anton_P <anton.paschke@st.ovgu.de>
2023-07-20 16:16:47 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
     public bool isGrounded = false;
     public LayerMask groundLayer;
+    private bool doubleJump;
 
     // Start is called before the first frame update
     private void Start()
@@ -28,51 +29,34 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxisRaw("Horizontal");
         rigidbody.velocity = new Vector2(dirX * 7f, rigidbody.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && (IsGrounded() || IsSpecial()) && !gameObject.GetComponent<ItemCollector>().shoe)
+        if (Input.GetButtonDown("Jump") && !gameObject.GetComponent<ItemCollector>().shoe)
         {
-
-            if (gameObject.GetComponent<PlayerInteraction>().gravity)
-            {
-                rigidbody.velocity = new Vector2(rigidbody.velocity.x, -8.7f);
-
-            }
-            else
+           
+            if (IsGrounded() || IsSpecial())
             {
                 rigidbody.velocity = new Vector2(rigidbody.velocity.x, 8.7f);
             }
-
         }
 
+        Debug.Log(doubleJump);
         if (Input.GetButtonDown("Jump") && gameObject.GetComponent<ItemCollector>().shoe)
         {
-            if (counter < 2)
+            if (IsGrounded() || IsSpecial() || doubleJump)
             {
-                if (gameObject.GetComponent<PlayerInteraction>().gravity)
-                {
-                    rigidbody.velocity = new Vector2(rigidbody.velocity.x, -10f);
-                    counter++;
-                }
-                else
-                {
-                    rigidbody.velocity = new Vector2(rigidbody.velocity.x, 10f);
-                    counter++;
-                }
-            }
-        } else if (IsGrounded() || IsSpecial())
-        {
-            counter = 0;
-        }
-
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, 10f);
+                doubleJump = !doubleJump;
+            }    
+        } 
     }
 
     // guckt ob man Boden mit Layer Ground berührt
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.up, 0.1f, jumpableGround) || Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
 
     private bool IsSpecial()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, specialGround) || Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.up, 0.1f, specialGround);
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, specialGround);
     }
 }
